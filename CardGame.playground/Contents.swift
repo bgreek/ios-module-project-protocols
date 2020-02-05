3836bfd62b4e3546c8495f0953a47444ec0a0cbf
Breena Greek <bgreek2019@gmail.com>
2020-02-04 23:33:57 -0500

    Almost finished project

--- CardGame.playground/Contents.swift
+++ CardGame.playground/Contents.swift
@@ -88,7 +88,7 @@ extension Card: CustomStringConvertible {
 //: ## Step 6
 //: Create a `struct` to model a deck of cards. It should be called `Deck` and have an array of `Card` objects as a constant property. A custom `init` function should be created that initializes the array with a card of each rank and suit. You'll want to iterate over all ranks, and then over all suits (this is an example of _nested `for` loops_). See the next 2 steps before you continue with the nested loops.
 struct Deck {
-    let cardArray: [Card] = []
+    var cardArray: [Card] = []
     init () {
         for suit in SuitOfAPlayingCard.allSuits {
             for rank in PlayingCard.allRanks {
@@ -96,8 +96,8 @@ struct Deck {
                 
             }
         }
+    }
 }
-
 
 //: ## Step 7
 //: In the rank enum, add a static computed property that returns all the ranks in an array. Name this property `allRanks`. This is needed because you can't iterate over all cases from an enum automatically.
@@ -118,29 +118,34 @@ struct Deck {
 //:
 //:}
 //:```
-
+// Done above
 
 
 //: ## Step 10
 //: These loops will allow you to match up every rank with every suit. Make a `Card` object from all these pairings and append each card to the `cards` property of the deck. At the end of the `init` method, the `cards` array should contain a full deck of standard playing card objects.
 
 
-
+// Done above
 
 
 //: ## Step 11
 //: Add a method to the deck called `drawCard()`. It takes no arguments and it returns a `Card` object. Have it draw a random card from the deck of cards and return it.
 //: - Callout(Hint): There should be `52` cards in the deck. So what if you created a random number within those bounds and then retrieved that card from the deck? Remember that arrays are indexed from `0` and take that into account with your random number picking.
-
-
-
-
+    extension Deck {
+        func drawCard() -> Card {
+            let randomCard = Int.random(in: 0..<cardArray.count)
+            return cardArray[randomCard]
+        }
+}
 
 //: ## Step 12
 //: Create a protocol for a `CardGame`. It should have two requirements:
 //: * a gettable `deck` property
 //: * a `play()` method
-
+protocol CardGame {
+    var deck: Deck { get }
+    func play ()
+}
 
 
 
@@ -148,16 +153,37 @@ struct Deck {
 //: Create a protocol for tracking a card game as a delegate called `CardGameDelegate`. It should have two functional requirements:
 //: * a function called `gameDidStart` that takes a `CardGame` as an argument
 //: * a function with the following signature: `game(player1DidDraw card1: Card, player2DidDraw card2: Card)`
-
-
+protocol CardGameDelegate {
+    func gameDidStart(game: CardGame)
+    func game(player1DidDraw card1: Card, player2DidDraw card2: Card)
+}
 
 
 //: ## Step 14
 //: Create a class called `HighLow` that conforms to the `CardGame` protocol. It should have an initialized `Deck` as a property, as well as an optional delegate property of type `CardGameDelegate`.
+class HighLow: CardGame {
+    var delegate: CardGameDelegate?
+    var deck: Deck = Deck()
+    func play() {
+        let player1 = deck.drawCard()
+        let player2 = deck.drawCard()
+        
+        if !(player1 < player2) && player1 != player2 {
+            print("Player 1 wins with \(player1.description)")
+        else if player 1 > player2 {
+            print("Player 2 wins with \(player2.description)")
+        else if player1 == player2 {
+            print("Round ends in a tie with ")
+                }
+            }
+        }
+    }
+}
 
 
-
-
+//Ends in a tie, something like, "Round ends in a tie with 3 of clubs."
+//Player 1 wins with a higher card, e.g. "Player 1 wins with 8 of hearts."
+//Player 2 wins with a higher card, e.g. "Player 2 wins with king of diamonds."
 //: ## Step 15
 //: As part of the protocol conformance, implement a method called `play()`. The method should draw 2 cards from the deck, one for player 1 and one for player 2. These cards will then be compared to see which one is higher. The winning player will be printed along with a description of the winning card. Work will need to be done to the `Suit` and `Rank` types above, so see the next couple steps before continuing with this step.
 
@@ -166,20 +192,32 @@ struct Deck {
 
 //: ## Step 16
 //: Take a look at the Swift docs for the [Comparable](https://developer.apple.com/documentation/swift/comparable) protocol. In particular, look at the two functions called `<` and `==`.
-
-
+// Done above
 
 
 //: ## Step 17
 //: Make the `Rank` type conform to the `Comparable` protocol. Implement the `<` and `==` functions such that they compare the `rawValue` of the `lhs` and `rhs` arguments passed in. This will allow us to compare two rank values with each other and determine whether they are equal, or if not, which one is larger.
-
-
+extension PlayingCard: Comparable {
+    static func < (lhs: PlayingCard, rhs: PlayingCard) -> Bool {
+        return lhs.rawValue < rhs.rawValue
+                }
+    static func == (lhs: PlayingCard, rhs: PlayingCard) -> Bool {
+        return lhs.rawValue == rhs.rawValue
+        }
+    }
 
 
 
 //: Step 18
 //: Make the `Card` type conform to the `Comparable` protocol. Implement the `<` and `==` methods such that they compare the ranks of the `lhs` and `rhs` arguments passed in. For the `==` method, compare **both** the rank and the suit.
-
+extension Card: Comparable {
+    static func < (lhs: Card, rhs: Card) -> Bool {
+        return lhs.rank < rhs.rank
+    }
+    static func == (lhs: Card, rhs: Card) -> Bool {
+        return (lhs.rank == rhs.rank && lhs.suit.rawValue == rhs.suit.rawValue)
+    }
+}
 
 
 
@@ -189,13 +227,25 @@ struct Deck {
 //: * Ends in a tie, something like, "Round ends in a tie with 3 of clubs."
 //: * Player 1 wins with a higher card, e.g. "Player 1 wins with 8 of hearts."
 //: * Player 2 wins with a higher card, e.g. "Player 2 wins with king of diamonds."
-
+// Done Above
 
 
 //: ## Step 20
 //: Create a class called `CardGameTracker` that conforms to the `CardGameDelegate` protocol. Implement the two required functions: `gameDidStart` and `game(player1DidDraw:player2DidDraw)`. Model `gameDidStart` after the same method in the guided project from today. As for the other method, have it print a message like the following:
 //: * "Player 1 drew a 6 of hearts, player 2 drew a jack of spades."
-
+class CardGameTracker: CardGameDelegate {
+    func gameDidStart(game: CardGame) {
+        if game is HighLow {
+            print("Started a new game of High Low")
+        }
+    }
+    
+    func game(player1DidDraw card1: Card, player2DidDraw card2: Card) {
+        print("Player 1 drew a \(card1.description), Player 2 drew a \(card2.description)")
+    }
+    
+    
+}
 
 
 //: Step 21
